35081cd079f7b84b525066a49c1dade9dbe16e01
abukowska <bukowska.joa@gmail.com>
2018-04-17 23:34:54 +0200

    Kwadraty excercise

---
+++ src/com/codeme/homework1/Kwadraty.java
@@ -0,0 +1,66 @@
+package com.codeme.homework1;
+
+public class Kwadraty {
+	
+	/**
+	 * Prints out:
+	 * 
+	 * ############
+	 *#          #
+	 *#          #
+	 *#  ######  #
+	 *#  #    #  #
+	 *#  #    #  #
+	 *#  #    #  #
+	 *#  #    #  #
+	 *#  ######  #
+	 *#          #
+	 *#          #
+	 *############
+	 *
+	 *Using 2 for loops
+	 */
+	
+	
+	public static void printSquareIntoSquare() {
+		
+		for(int y = 1; y < 13; y++) {
+			for(int x = 1; x < 13; x++) {
+				if (y==1 || y==12) {
+					System.out.print("#");
+					continue;
+				}
+				if (y==2 || y==3 || y==10 || y==11) {
+					if (x==1 || x==12) {
+						System.out.print("#");
+					} else {
+						System.out.print(" ");
+					}
+				} else { 
+					if (y==4 || y==9) {
+						if (x==1 || x==4 || x==5 || x==6 || x==7 || x==8 || x==9 || x==12) {
+							System.out.print("#");
+						} else {
+							System.out.print(" ");
+						}
+					} else {
+						if (x==1 || x==4 || x == 9 || x==12) {
+							System.out.print("#");
+						} else {
+							System.out.print(" ");
+						}
+					}
+				}
+			}
+			System.out.println();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		printSquareIntoSquare();
+
+	}
+
+}
