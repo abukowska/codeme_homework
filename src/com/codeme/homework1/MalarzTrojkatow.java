b472ef942c4a3d50c27fa7503181b772b9977c8c
abukowska <bukowska.joa@gmail.com>
2018-04-17 22:21:18 +0200

    MalazTrojkatow code v1.0

--- src/com/codeme/homework1/MalarzTrojkatow.java
+++ src/com/codeme/homework1/MalarzTrojkatow.java
@@ -4,7 +4,26 @@ public class MalarzTrojkatow {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		for(int y = 1; y < 4; y++) {
+			for(int x = 1; x < 4; x++) {
+				if (x==1) {
+					System.out.print("/");
+					continue;
+				}
+				if(y == 1) {
+					if(x == 2) {
+						System.out.println("\\");
+					}
+				} 
+				else {
+					if(x == 3) {
+						System.out.println("\\");
+					} else {
+						System.out.print(" ");
+					}
+				}
+			}
+		}
 	}
 
 }
