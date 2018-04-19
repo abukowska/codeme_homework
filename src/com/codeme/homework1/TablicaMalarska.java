5bf48ba940542abf7e482f8df3cd97d5f7bfc6a0
abukowska <bukowska.joa@gmail.com>
2018-04-19 23:00:03 +0200

    adding changes to TablicaMalarska HW1

--- src/com/codeme/homework1/TablicaMalarska.java
+++ src/com/codeme/homework1/TablicaMalarska.java
@@ -1,14 +1,28 @@
 package com.codeme.homework1;
 
 public class TablicaMalarska {
-
-	public static void main(String[] args) {
+	
+	/**
+	 * It prints out:
+	 *   *
+	 *   
+	 *   * *
+	 *
+	 *   * *
+	 *
+	 *   * *
+	 *   * * 
+	 *
+	 * using 2 for loops and multidimensional arrays
+	 */
+	
+	public static void paint() {
 		
-		String[][] tablicaMalarska = new String[5][2];
+		String[][] tablicaMalarska = new String[8][2];
 		
 		for(int a = 0; a < tablicaMalarska.length; a++) {
 			for(int b = 0; b < tablicaMalarska[a].length; b++) {
-				if (a==0 && b==1) {
+				if (a==0 && b==1 || a==1 || a==3 || a==5) {
 					tablicaMalarska[a][b] = " ";
 				} else {
 					tablicaMalarska[a][b] = "#";
@@ -19,13 +33,20 @@ public class TablicaMalarska {
 		for(String[] inside_array : tablicaMalarska) {
 			for(String element : inside_array) {
 				if (element == tablicaMalarska[0][1]) {
-					System.out.print(" " + "  ");
+					System.out.print(" " + " ");
 				} else {
-					System.out.print("*" + "  ");
+					System.out.print("*" + " ");
 				}
 			}
 			System.out.println();
 		}
+	}
+	
+
+	public static void main(String[] args) {
+		
+		paint();
+		
 	}
 
 }
