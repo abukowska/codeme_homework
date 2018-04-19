ed10c85452509caadbe4066dfe1469551294904e
abukowska <bukowska.joa@gmail.com>
2018-04-19 21:54:33 +0200

    MalarzTrojkatow refator

--- src/com/codeme/homework1/MalarzTrojkatow.java
+++ src/com/codeme/homework1/MalarzTrojkatow.java
@@ -2,37 +2,42 @@ package com.codeme.homework1;
 
 public class MalarzTrojkatow {
 	
-	/** Prints out
-	 * /\
-	 * / \
-	 * / \
-	 * using while loop
+	/** Prints out:
+	 * 
+	 *     /\
+	 *    /  \
+	 * 	 /    \
+	 * 	 ------
+	 * 
+	 * using 2 while loops
 	 */
 	public static void printTrianglesWhile() {
+		
 		int y = 1;
-		while (y < 4) {
-			for(int x = 1; x < 4; x++) {
-				if (x==1) {
+		while(y < 4) {
+			int x = 1;
+			while(x < 4) {
+				if(x==1) {
 					System.out.print("/");
-					continue;
 				}
-				if(y == 1) {
-					if(x == 2) {
+				if(y==1) {
+					if(x==2) {
 						System.out.println("\\");
 					}
 				} 
 				else {
-					if(x == 3) {
-						System.out.println("\\");
-					} else {
+					if(x==2) {
 						System.out.print(" ");
+					} else if (x==3) {
+						System.out.println("\\");
 					}
 				}
+				x++;
 			}
 			y++;
 		}
 	}
-	
+
 	
 	public static void main(String[] args) {
 		
