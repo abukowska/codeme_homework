9b85e62ea52956dd7475ba6743b1cacc91d6b894
abukowska <bukowska.joa@gmail.com>
2018-04-19 22:24:53 +0200

    refactor pf MalarzTrojkatow

--- src/com/codeme/homework1/MalarzTrojkatow.java
+++ src/com/codeme/homework1/MalarzTrojkatow.java
@@ -14,22 +14,42 @@ public class MalarzTrojkatow {
 	public static void printTrianglesWhile() {
 		
 		int y = 1;
-		while(y < 4) {
+		while(y <= 4) {
 			int x = 1;
-			while(x < 4) {
-				if(x==1) {
-					System.out.print("/");
-				}
+			while(x <= 7) {
 				if(y==1) {
-					if(x==2) {
-						System.out.println("\\");
-					}
-				} 
-				else {
-					if(x==2) {
+					if(x==3) {
+						System.out.print("/");
+					} else if (x==4) {
+						System.out.print("\\");
+					} else {
 						System.out.print(" ");
-					} else if (x==3) {
-						System.out.println("\\");
+					}
+				} else if (y==2) {
+					if(x==1) {
+						System.out.println("");
+						System.out.print(" ");
+					} else if (x==2) {
+						System.out.print("/");
+					} else if (x==5) {
+						System.out.print("\\");
+					} else {
+						System.out.print(" ");
+					}
+				} else if (y==3) {
+					if(x==1) {
+						System.out.println("");
+						System.out.print("/");
+					} else if (x==6) {
+						System.out.print("\\");
+					} else {
+						System.out.print(" ");
+					}
+				} else {
+					if (x==1) {
+						System.out.println("");
+					} else {
+						System.out.print("-");
 					}
 				}
 				x++;
