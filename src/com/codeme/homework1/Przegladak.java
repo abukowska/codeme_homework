7cd6fb0a8582106e71626010be6f3f88e247b606
abukowska <bukowska.joa@gmail.com>
2018-05-09 22:18:36 +0200

    removing unnecessary imports for Przegladak

--- src/com/codeme/homework1/Przegladak.java
+++ src/com/codeme/homework1/Przegladak.java
@@ -9,10 +9,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
+
 
 public class Przegladak {
 	
