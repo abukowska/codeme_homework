1cb559aab92cce7c2501b9c7178dfec4ee6cff5a
abukowska <bukowska.joa@gmail.com>
2018-05-13 16:35:12 +0200

    BasicEngma HW2 fix for operating on more than one-line text doc

--- src/com/codeme/homework3/enigma/BasicEngima.java
+++ src/com/codeme/homework3/enigma/BasicEngima.java
@@ -12,8 +12,10 @@ import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 
 public class BasicEngima {
+	
 	private File file;
 	private File copyFile;
+	StringBuilder encodedContent;
 
 	public void copyExistingFileAndEncrypt(String absolutePath) {
 		file = new File("/home/jzawadzka/text_file/textfile");
@@ -23,6 +25,7 @@ public class BasicEngima {
 				try {
 					Files.copy(file.toPath(), copyFile.toPath());
 					codeCopiedFile();
+					System.out.println("File safely encrypted and stored under location: " + copyFile.getAbsolutePath() + ":\n");
 					break;
 				} catch (FileAlreadyExistsException foe) {
 					copyFile.delete();
@@ -32,41 +35,63 @@ public class BasicEngima {
 				}
 			}
 		}
-		// codeCopiedFile();
 	}
 
 	private void codeCopiedFile() throws IOException {
-		StringBuilder sb = new StringBuilder();
+		encodedContent = new StringBuilder();
 		while (true) {
 			try (BufferedReader bReader = new BufferedReader(new InputStreamReader(new FileInputStream(copyFile)))) {
 				String line;
-
 				while ((line = bReader.readLine()) != null) {
 					for (char oneCharacterInLine : line.toCharArray()) {
-						sb.append(oneCharacterInLine += 2);
+						encodedContent.append(oneCharacterInLine += 2);
 					}
-					sb.append("\n");
+					encodedContent.append("\n");
 				}
 				break;
-
 			} catch (IOException ioe) {
 				ioe.printStackTrace();
 			}
 		}
-
 		try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(copyFile)))) {
 			writer.write("");
-			writer.write(sb.toString());
+			writer.write(encodedContent.toString());
 			writer.flush();
 		}
 	}
 
+	public void decryptPreviouslyEncryptedCopyOfFileAndOverwrite() {
+		if (copyFile.exists()) {
+			while (true) {
+				try (BufferedReader bReader = new BufferedReader(new InputStreamReader(new FileInputStream(copyFile))); 
+						BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(copyFile)))) {
+					StringBuilder decodedContent = new StringBuilder();
+					String[] splittedEncContentLines = encodedContent.toString().split("\n");		
+					for(String oneLine : splittedEncContentLines) {
+						for(char oneChar : oneLine.toCharArray()) {
+							decodedContent.append(oneChar -= 2);
+						}
+						decodedContent.append("\n");
+					}
+					writer.write(decodedContent.toString());
+					writer.flush();
+					break;
+				} catch (IOException ioe) {
+					ioe.printStackTrace();
+				}
+			}		
+			System.out.println("File " + copyFile.getName() + "  safely decrypted and overwritten.\n");
+		}
+	}
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		String filePath = "/home/jzawadzka/text_file/textfile";
 		BasicEngima enigma = new BasicEngima();
 		enigma.copyExistingFileAndEncrypt(filePath);
-
+		//enigma.decryptPreviouslyEncryptedCopyOfFileAndOverwrite();
 	}
 
 }
