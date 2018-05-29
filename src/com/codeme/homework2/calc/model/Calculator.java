ebaa4819f0b02b8503b44bb684a1ccaa2f6d86ac
abukowska <bukowska.joa@gmail.com>
2018-05-30 00:26:21 +0200

    introducing enum to calc

--- src/com/codeme/homework2/calc/model/Calculator.java
+++ src/com/codeme/homework2/calc/model/Calculator.java
@@ -5,28 +5,36 @@ import java.util.regex.Pattern;
 
 public class Calculator {
 	
+	public enum MathOperations {
+		ADD("+"), SUBTRACT("-"), MULTIPLY("*"), DIVIDE("/");
+		
+		private String sign;
+		
+		private MathOperations(String sign) {
+			this.sign = sign;
+		}
+	}
+	
 	public void calculate(String usersEquation) {
 		String pattern = "^((\\-)?(\\d+)(\\.\\d++)?)(\\+|\\-|\\/|\\*)((\\-)?(\\d+)(\\.\\d++)?)$";
 		Pattern r = Pattern.compile(pattern);
 		Matcher m = r.matcher(usersEquation);
 
 		if (m.find()) {
-			switch (m.group(5)) {
-			case "+":
+			MathOperations operSign = getOperationNameBySign(m.group(5));
+			
+			switch (operSign) {
+			case ADD:
 				System.out.println(add(Double.valueOf(m.group(1)), Double.valueOf(m.group(6))));
 				break;
-			case "-":
+			case SUBTRACT:
 				System.out.println(subtract(Double.valueOf(m.group(1)), Double.valueOf(m.group(6))));
 				break;
-			case "*":
-				System.out.println(multiple(Double.valueOf(m.group(1)), Double.valueOf(m.group(6))));
+			case MULTIPLY:
+				System.out.println(multiply(Double.valueOf(m.group(1)), Double.valueOf(m.group(6))));
 				break;
-			case "/":
-				//try {
-					System.out.println(divide(Double.valueOf(m.group(1)), Double.valueOf(m.group(6))));
-/*				} catch (ArithmeticException ae) {
-					System.out.println("Can't divide by zero.");
-				}*/
+			case DIVIDE:
+				System.out.println(divide(Double.valueOf(m.group(1)), Double.valueOf(m.group(6))));
 				break;
 			default:
 				System.out.println("");
@@ -34,6 +42,15 @@ public class Calculator {
 		} else {
 			throw new IllegalArgumentException();
 		}
+	}
+	
+	private MathOperations getOperationNameBySign(String sign) {
+		for(MathOperations operationName : MathOperations.values()) {
+			if(operationName.sign.equals(sign)) {
+				return operationName;
+			}
+		}
+		return null;
 	}
 	
 	private Double add(Double a, Double b) {
@@ -44,7 +61,7 @@ public class Calculator {
 		return a-b;
 	}
 	
-	private Double multiple(Double a, Double b) {
+	private Double multiply(Double a, Double b) {
 		return a*b;
 	}
 	
@@ -54,4 +71,5 @@ public class Calculator {
 		}
 		return a/b;
 	}
+	
 }
